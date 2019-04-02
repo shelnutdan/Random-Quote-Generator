b87f5d780a21837f982c6496ed800fa0c01fefa5
shelnutdan <shelnutdaniel@gmail.com>
2019-04-02 09:13:50 -0700

    Fourth Commit: removed unneeded console.logs and removed blank spacing. Added README.md for documentation

---
+++ README.md
@@ -0,0 +1,20 @@
+# Random Quote Generator
+This project makes an api call on load of the window to the forismatic api and retrieves a random quote with its corresponding author. When the **New Quote** has been clicked, a new call to the forimatic api will be made and a random quote with its author will appear on the screen, and you will have the option to share this quote on twitter. Additionally, every request made by clicking the **New Quote** button the background color will randomly change.
+
+
+
+## Skills
+- HTML5
+- CSS3
+- JavaScript
+- jQuery
+- Asynchronous JavaScript
+
+
+## Cloning this repo
+```
+$ git https://github.com/shelnutdan/Random-Quote-Generator.git
+```
+
+## How to use this app
+Just click on the **New Quote** button and get a random quote now.
--- js/app.js
+++ js/app.js
@@ -1,29 +1,27 @@
+/*URL that the application uses to pull quote*/
 let url="https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?"
 
-$(window).on('load',function(){
-  //let url="https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?"
-
-  $.getJSON(url,function(data){
-    //console.log(data.quoteText)
-    //console.log(data.quoteAuthor)
-    //console.log(data)
-    $('body').css('background-color',randomRGBAColor())
-    document.getElementById("text").innerHTML=data.quoteText
-    document.getElementById("author").innerHTML="-"+data.quoteAuthor
-  },'jsonp');
-})
+/*Click functions*/
 $(document).ready(function(){
-  $('.btn-primary').on('click',function(e){
+  /* Pulls a random  quote and its author on the page and sets random background color*/
+  $.getJSON(url,function(data){
+
+      $('body').css('background-color',randomRGBAColor())
+      document.getElementById("text").innerHTML=data.quoteText
+      document.getElementById("author").innerHTML="-"+data.quoteAuthor
+  },'jsonp');
+  /*When the get quote button is clicked then a random quote and its author is generated*/
+  $('.quote_button').on('click',function(e){
     $.getJSON(url,function(data){
-      //console.log(data.quoteText)
-      //console.log(data.quoteAuthor)
-      //console.log(data)
+
       $('body').css('background-color',randomRGBAColor())
       document.getElementById("text").innerHTML=data.quoteText
       document.getElementById("author").innerHTML="-"+data.quoteAuthor
     },'jsonp');
+    console.log(randomRGBAColor())
 
   })
+  /*Posts the quote and author to tweeter*/
   $('#twitter').on('click',function(){
     let myUrl='https://twitter.com/intent/tweet?text=' + document.getElementById("text").innerHTML+"-"+document.getElementById("author").innerHTML
     window.open(myUrl, '#twitter');
@@ -31,14 +29,12 @@ $(document).ready(function(){
 
 });
 
-
+/*When a random quote is generated a random color is slected to be set*/
 function randomRGBAColor(){
   let redColor=Math.floor(Math.random()*255)
   let greenColor=Math.floor(Math.random()*255)
   let blueColor=Math.floor(Math.random()*255)
-  let alpha=Math.random()
+  let alpha=Math.random().toFixed(1)
 
   return `rgba(${redColor},${greenColor},${blueColor},${alpha})`;
 }
-
-console.log(randomRGBAColor())
